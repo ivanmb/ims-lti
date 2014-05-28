9f2e9f9c92a356a211762f9f98434fbb020bd442
Iván Badgen <ivanbadgen@gmail.com>
2014-05-28 16:47:00 -0300

    First test on dynamic class

--- src/IMS/LTI/LaunchParams.php
+++ src/IMS/LTI/LaunchParams.php
@@ -1,11 +1,26 @@
-<?php 
+<?php
 
 namespace IMS\LTI;
 
 class LaunchParams {
+
+	protected $settings = array ();
+
+	public function __call($methodName, $params = null) {
+		$methodPrefix = substr($methodName, 0, 3);
+		$key = strtolower(substr($methodName, 3));
+		
+		if ($methodPrefix == 'set' && count($params) == 1) {
+			$value = $params [0];
+			$this->settings [$key] = $value;
+			
+		} elseif ($methodPrefix == 'get') {
+			if (array_key_exists($key, $this->settings))
+				return $this->settings[$key];
+			
+		} else {
+			throw new \InvalidArgumentException("The method $methodName was not found");
+		}
 	
-	public static function sayHello() {
-		return "Hello!";
 	}
-	
 } 
--- tests/IMS/LTI/Tests/LaunchParamsTest.php
+++ tests/IMS/LTI/Tests/LaunchParamsTest.php
@@ -6,8 +6,26 @@ use IMS\LTI\LaunchParams;
 
 class LaunchParamsTest extends \PHPUnit_Framework_TestCase {
 	
-	public function test_sayHello() {
-		$this->assertEquals("Hello!", LaunchParams::sayHello());
+	public function testSetSomething_Ok() {
+		$params = new LaunchParams();
+		$params->setSomething("Hello!");
+		$params->setOther(10);
+		
+		$this->assertEquals("Hello!", $params->getSomething());
+		$this->assertEquals(10, $params->getOther());
+	}
+	
+	public function testGetUndefined_null() {
+		$params = new LaunchParams();
+		$this->assertNull($params->getSomething());
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testUnknownMethod_InvalidArgument() {
+		$params = new LaunchParams();
+		$params->unexistent();
 	}
 	
 } 

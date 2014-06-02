9b92e738cb1508c5a461b7b1e619ee4a80ff8be9
Iván Badgen <ivanbadgen@gmail.com>
2014-06-02 15:22:56 -0300

    Roles capitalized

--- src/IMS/LTI/Roles.php
+++ src/IMS/LTI/Roles.php
@@ -3,19 +3,19 @@
 namespace IMS\LTI;
 
 abstract class Roles extends EnumParam {
-	const STUDENT 				= 'student';
-	const FACULTY 				= 'faculty';
-	const MEMBER 				= 'member';
-	const LEARNER 				= 'learner';
-	const INSTRUCTOR 			= 'instructor';
-	const MENTOR 				= 'mentor';
-	const STAFF 				= 'staff';
-	const ALUMNI 				= 'alumni';
-	const PROSPECTIVE_STUDENT 	= 'prospectivestudent';
-	const GUEST 				= 'guest';
-	const OTHER 				= 'other';
-	const ADMINISTRATOR 		= 'administrator';
-	const OBSERVER 				= 'observer';
-	const NONE 					= 'none';
+	const STUDENT 				= 'Student';
+	const FACULTY 				= 'Faculty';
+	const MEMBER 				= 'Member';
+	const LEARNER 				= 'Learner';
+	const INSTRUCTOR 			= 'Instructor';
+	const MENTOR 				= 'Mentor';
+	const STAFF 				= 'Staff';
+	const ALUMNI 				= 'Alumni';
+	const PROSPECTIVE_STUDENT 	= 'Prospectivestudent';
+	const GUEST 				= 'Guest';
+	const OTHER 				= 'Other';
+	const ADMINISTRATOR 		= 'Administrator';
+	const OBSERVER 				= 'Observer';
+	const NONE 					= 'None';
 	
 }
